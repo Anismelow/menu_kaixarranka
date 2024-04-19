3db428f92352e9920509cf304407d5e2096c44b2
chuans <juaazn.05@gmail.com>
2024-04-19 15:19:07 +0200

    Componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header () {
+  return (
+    <header className='w-screen h-auto flex justify-center my-8'>
+      <h1 className='font-bold text-5xl text-center text-amarillo'>Kaixarranka taberna</h1>
+    </header>
+  )
+}
